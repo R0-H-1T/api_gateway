15575089b6878caba30bab59291aa13557e5844d
rohit <rowhittank@gmail.com>
2024-10-05 19:52:12 +0530

    setup qna endpoints forwarding

--- app/qna/qna.py
+++ app/qna/qna.py
@@ -1,19 +1,57 @@
-from fastapi import APIRouter
-from ..helper import Tags
+from typing import Annotated
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from app.helper import Tags
+from app.dependency import get_async_client
+from app.quiz_models.qna_models import Questionaire, Answers
+from httpx import AsyncClient
 
 router = APIRouter(tags=[Tags.qna])
 
-
-@router.post("/question")
-async def question():
-    return {"data": "question"}
+prefix_url = "http://localhost:8083"
 
 
-@router.post("/answer")
-async def answer():
-    return {"data": "answer"}
+@router.post("/question", status_code=status.HTTP_201_CREATED)
+async def question(
+    questionnaire: Questionaire,
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer())],
+    client: Annotated[AsyncClient, Depends(get_async_client)],
+):
+    res = await client.post(
+        url=f"{prefix_url}/question",
+        headers={"Authorization": f"{credentials.scheme} {credentials.credentials}"},
+        json=questionnaire.model_dump(),
+    )
+
+    if res.status_code != status.HTTP_201_CREATED:
+        raise HTTPException(status_code=res.status_code)
 
 
-@router.get("/questionnaire/{qna_id}")
-async def get_qna(qna_id: int):
-    pass
+@router.post("/answer", status_code=status.HTTP_201_CREATED)
+async def answer(
+    answer: Answers,
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer())],
+    client: Annotated[AsyncClient, Depends(get_async_client)],
+):
+    res = await client.post(
+        url=f"{prefix_url}/answer",
+        headers={"Authorization": f"{credentials.scheme} {credentials.credentials}"},
+        json=answer.model_dump(),
+    )
+
+    if res.status_code != status.HTTP_201_CREATED:
+        raise HTTPException(status_code=res.status_code)
+
+
+@router.get("/questionnaire/{qna_id}", status_code=status.HTTP_200_OK)
+async def get_qna(
+    qna_id: int,
+    credentials: Annotated[HTTPAuthorizationCredentials, Depends(HTTPBearer())],
+    client: Annotated[AsyncClient, Depends(get_async_client)],
+):
+    res = await client.get(
+        url=f"{prefix_url}/get_qna/{qna_id}",
+        headers={"Authorization": f"{credentials.scheme} {credentials.credentials}"},
+    )
+
+    return res.json()
